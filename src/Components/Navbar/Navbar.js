84ece879fcd1f84bac95edadd59f008bda254cef
Micke Berg <micke.berg.dev@gmail.com>
2021-01-30 18:10:24 +0100

    Add filter functionality

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -2,18 +2,25 @@ import React, { useState } from 'react';
 import './Navbar.scss';
 import Logo from '../Logo/Logo';
 import Search from '../Search/Search';
+import Filter from '../Filter/Filter';
+
 import CartDropdown from '../CartDropdown/CartDropdown';
 import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 
-const Navbar = () => {
+const Navbar = ({ count, price, sortMovies, filterMovies }) => {
   const [cart, setCart] = useState([])
   return (
     <nav className="navbar-container">
       <div className="nav-element-left">
         <Logo />
         <button className="button-tertiary button-tertiary-active" label="Home">Home</button>
-        <button className="button-tertiary" label="Categories">Genres</button>
+        <Filter  
+        count={count} 
+        price={price}
+        filterMovies={filterMovies}
+        sortMovies={sortMovies}
+        />
       </div>
       <div className="nav-element-right">
         {/* <Search /> */}
