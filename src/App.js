20f0defcd1c06234ff88d496fa1651f07b174a56
Micke Berg <micke.berg.dev@gmail.com>
2021-01-30 13:07:54 +0100

    Add Hero, Header, Product, RadomMovie

--- src/App.js
+++ src/App.js
@@ -1,16 +1,47 @@
+import React, { useState, useEffect } from 'react'
+
 import Footer from "./Components/Footer/Footer";
 import GridContainer from "./Components/GridContainer/GridContainer";
 import Header from "./Components/Header/Header";
+import Hero from './Components/Hero/Hero';
+import Products from './Components/Products/Products';
+import data from "./data.json";
 
 function App() {
+  const movies = data.movies;
+  const [random, setRandom] = useState(0);
+
+  useEffect(() => {
+    if (movies.length > 0) {
+      const randomMovieIndex = Math.floor(Math.random() * (movies.length - 1));
+      const randomMovie = movies[randomMovieIndex];
+      setRandom(randomMovie.id);
+    }  
+  }, []);
+  
+  
   return (
-    <GridContainer>
+    <>
       <Header />
-      <main className="main">
-        Product List
-      </main>
-      <Footer />
-    </GridContainer>
+      <GridContainer>
+        <Hero 
+          title={movies[random].title} 
+          backgroundImage={movies[random].image} 
+          description={movies[random].description}
+          genres={movies[random].movieGenre}
+          releaseDate={movies[random].releaseDate}
+          />
+        <main className="main">
+          <div className="content">
+            <div className="main">
+            <Products movies={movies}/>
+            </div>
+            <div className="sidebar">Cart Items</div>
+          </div>
+        </main>
+        <Footer />
+      </GridContainer>
+    </>
   );
 }
 
