4e5a93d9d98240b8083a9ae0d4d67eeabf26c727
Micke Berg <micke.berg.dev@gmail.com>
2021-02-01 21:28:48 +0100

    Add CartComponent. Pass props

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,35 +1,41 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './Navbar.scss';
 import Logo from '../Logo/Logo';
 import Search from '../Search/Search';
 import Filter from '../Filter/Filter';
+import Cart from '../Cart/Cart';
 
-import CartDropdown from '../CartDropdown/CartDropdown';
-import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
-import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+const Navbar = ({ 
+  price, 
+  sortMovies, 
+  filterMovies, 
+  totalQuantity,
+  removeFromCart,
+  createOrder,
+  cartItems }) => {
 
-const Navbar = ({ count, price, sortMovies, filterMovies }) => {
-  const [cart, setCart] = useState([])
   return (
     <nav className="navbar-container">
       <div className="nav-element-left">
         <Logo />
-        <button className="button-tertiary button-tertiary-active" label="Home">Home</button>
+        <a href="/">Home</a>
         <Filter  
-        count={count} 
-        price={price}
-        filterMovies={filterMovies}
-        sortMovies={sortMovies}
+          price={price}
+          filterMovies={filterMovies}
+          sortMovies={sortMovies}
         />
       </div>
       <div className="nav-element-right">
         {/* <Search /> */}
-        <button className="notifications-menu">
-          <ShoppingCartIcon fontSize="medium"/>
-          <span className="notification-pill">9</span>
-        </button>
-        <ArrowDropDownIcon />
-        {/* <CartDropdown >{cart}</CartDropdown> */}
+        <Cart 
+          cartItems={cartItems} 
+          removeFromCart={removeFromCart}
+          totalQuantity={totalQuantity} 
+          createOrder={createOrder}
+        />
+        <a href="#admin">
+          <div className="custom-arrow3"></div>
+        </a>
       </div>
     </nav>
   )
