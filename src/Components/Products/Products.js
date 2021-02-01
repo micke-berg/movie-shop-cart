06485fa2d5132037c4d37a2f0a4469987937612c
Micke Berg <micke.berg.dev@gmail.com>
2021-02-01 21:30:04 +0100

    onClick addToCart. Change of classNames

--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Products.scss';
 import Button from '../Button/Button';
 
-const Products = ({ filtering, movies, genreTitle }) => {
+const Products = ({ filtering, movies, genreTitle, addToCart }) => {
   const truncate = (str, n) => {
     return str?.length > n ? str.substring(0, n - 1) + "..." : str;
   };
@@ -17,11 +17,11 @@ const Products = ({ filtering, movies, genreTitle }) => {
               <a href={`#${movie.id}`}>
                 <img src={movie.image} alt={movie.title}/>
               </a>
-              <div className="movie-price">
+              <div className="movie-card-info">
                 <p>{truncate(movie.title, 22)}</p>
                 <div>
-                  <Button label="Add to Cart" className="btn btn-primary">Add to Cart</Button>
-                  <div >$ {movie.price}</div>
+                  <Button onClick={() => addToCart(movie)} label="Add to Cart" className="btn btn-primary">Add to Cart</Button>
+                  <div className="movie-price">$ {movie.price}</div>
                   </div>
               </div>
             </div>
