1427c6683ecc7d70dd5bc45d16e6e81a5657bca3
Micke Berg <micke.berg.dev@gmail.com>
2021-02-03 10:04:37 +0100

    Add Modal

--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Modal from "react-modal";
+import { Fade } from 'react-awesome-reveal';
 import './Products.scss';
 import Button from '../Button/Button';
+import Rating from '../Rating/Rating';
+import GenreTags from '../GenreTags/GenreTags';
 
 const Products = ({ filtering, movies, genreTitle, addToCart }) => {
+  const [modalProduct, setModalProduct] = useState(null);
+
   const truncate = (str, n) => {
     return str?.length > n ? str.substring(0, n - 1) + "..." : str;
   };
+
+  const openProductModal = (movie) => {
+    setModalProduct(movie);
+  }
+
+  const closeProductModal = () => { 
+    // clearOrder();
+    setModalProduct(null);
+  };
+  console.log('object', modalProduct)
 
   return (
     <div className="movie-section">
@@ -15,10 +31,10 @@ const Products = ({ filtering, movies, genreTitle, addToCart }) => {
           <li key={movie.id} >
             <div className="movie">
               <a href={`#${movie.id}`}>
-                <img src={movie.image} alt={movie.title}/>
+                <img onClick={() => openProductModal(movie)} src={movie.image} alt={movie.title}/>
               </a>
               <div className="movie-card-info">
-                <p>{truncate(movie.title, 22)}</p>
+                <p>{truncate(movie.title, 24)}</p>
                 <div>
                   <Button onClick={() => addToCart(movie)} label="Add to Cart" className="btn btn-primary">Add to Cart</Button>
                   <div className="movie-price">$ {movie.price}</div>
@@ -28,6 +44,56 @@ const Products = ({ filtering, movies, genreTitle, addToCart }) => {
           </li>
         ))}
       </ul>
+      {modalProduct && 
+        <Modal isOpen={true} onRequestClose={closeProductModal}
+        style={{
+          overlay: {
+            position: "fixed",
+            zIndex: "200",
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            backgroundColor: "rgba(0, 0, 0, 0.75)",
+            overflowY: "auto",
+          },
+          content: {
+            position: "absolute",
+            display: "flex",
+            flexWrap: "wrap",
+            justifyContent: "center",
+            top: "50%",
+            left: "50%",
+            "transform": "translate(-50%, -50%)",
+            height: "68%",
+            minHeight: "400px",
+            margin: "10px 0",
+            width: "66vw",
+            border: "none",
+            background: "#242424",
+            padding: "2%",
+          },
+        }}
+        >
+          <Fade duration={200}>
+            <div className="modal-wrapper">
+              <button onClick={() => closeProductModal() } className="close-modal" ></button>
+              <div className="movie-details">
+                <img src={modalProduct.image} alt={modalProduct.title}/>      
+                <div className="movie-details-description">
+                  <div>
+                      <div className="movie-details-title">{modalProduct.title}</div>
+                  <p>{modalProduct.releaseDate}</p>
+                  </div>
+                    <GenreTags genres={modalProduct.movieGenre}/>
+                    <Rating rating={modalProduct.rating}/>
+                  <p>{truncate(modalProduct.description, 280)}</p>
+                  <button onClick={() => {addToCart(modalProduct); closeProductModal();}} className="modal-button">Add to cart</button>
+                </div>
+              </div>
+            </div>
+          </Fade>
+        </Modal>}
     </div>
   )
 }
