9fc2bece152bc423b081d5d17d2419f3d9894ee7
Micke Berg <micke.berg.dev@gmail.com>
2021-02-03 10:04:13 +0100

    ClickOutside Component

---
+++ src/Components/ClickOutside/ClickOutside.js
@@ -0,0 +1,26 @@
+import { useEffect, useRef } from 'react'
+
+function useOutsideAlerter(ref) {
+  useEffect(() => {
+
+  function handleClickOutside(event) {
+    if (ref.current && !ref.current.contains(event.target)) {
+      alert("You clicked outside of me!");
+      }
+  }
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [ref]);
+}
+
+// Component wrapper if you click outside
+function ClickOutside(props) {
+  const wrapperRef = useRef(null);
+  useOutsideAlerter(wrapperRef);
+
+  return <div ref={wrapperRef}>{props.children}</div>;
+}
+
+export default ClickOutside;
