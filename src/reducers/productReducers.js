9323f87ac36ccf8900e46cf925f12938e07b7e26
Micke Berg <micke.berg.dev@gmail.com>
2021-03-10 20:33:22 +0100

    Redux

---
+++ src/reducers/productReducers.js
@@ -0,0 +1,43 @@
+import {
+  FETCH_PRODUCTS,
+  FETCH_GENRES,
+  FILTER_PRODUCTS_BY_GENRE,
+  FILTER_BY_SEARCH,
+  ORDER_PRODUCTS_BY_PRICE,
+} from "../types";
+
+export const productsReducer = (state = {}, action) => {
+  switch (action.type) {
+    case FILTER_PRODUCTS_BY_GENRE:
+      return {
+        ...state,
+        genre: action.payload.genre,
+        filteredItems: action.payload.items,
+      };
+    case FILTER_BY_SEARCH:
+      return {
+        ...state,
+        searchResult: action.payload.searchTerm,
+        filteredItems: action.payload.items,
+      };
+    case ORDER_PRODUCTS_BY_PRICE:
+      return {
+        ...state,
+        sort: action.payload.sort,
+        filteredItems: action.payload.items,
+      };
+    case FETCH_PRODUCTS:
+      return { 
+        items: action.payload, 
+        filteredItems: action.payload,
+        movies: action.payload,
+        success: action.payload,
+      };
+    case FETCH_GENRES:
+      return { 
+        genres: action.payload, 
+      };
+    default:
+      return state;
+  }
+};
