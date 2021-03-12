b30664fee2bd14b099d53c22fd928b1d46da6d30
Micke Berg <micke.berg.dev@gmail.com>
2021-03-12 10:34:09 +0100

    New syling, animations and content

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -2,10 +2,10 @@ import React, { useState, useEffect, useRef } from "react";
 import './Search.scss';
 import { connect } from 'react-redux';
 import { filterSearch } from '../../actions/productActions';
-import SearchIcon from "@material-ui/icons/Search";
+import SearchIcon from '@material-ui/icons/Search';
 
 const Search = (props) => {
-  const [state, setState] = useState("");
+  const [searchTermState, setSearchTermState] = useState("");
   const initial = useRef(true);
 
   useEffect(() => {
@@ -15,28 +15,30 @@ const Search = (props) => {
     }
 
     const timer = setTimeout(() => {
-      props.filterSearch(state);
+      props.filterSearch(searchTermState);
     }, 500);
 
     return () => clearTimeout(timer);
-  }, [state]);
-  
-  return (
+  }, [searchTermState]);
 
-    <div className="search">
-          <SearchIcon />
-        <form className='searchbar-content'>
-          <input
-            type="search"
-            placeholder="Search for a title..."
-            onChange={(event) => setState(event.currentTarget.value)}
-            value={state}
-          />
-      </form>
+  return (
+    <div className="search-content">
+      <div className="search">
+        <input 
+          type="text" 
+          className="search__input" 
+          aria-label="search" 
+          placeholder="Titles, genres..."
+          onChange={(event) => setSearchTermState(event.currentTarget.value)}
+          value={searchTermState}
+        ></input>
+        <button className="search__button" aria-label="submit search">
+          <SearchIcon fontSize="medium" className="search-icon" />
+        </button>
+      </div>
     </div>
   )
 }
-
 // export default Search
 
 export default connect(
