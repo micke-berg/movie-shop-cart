99b55e73dedf8c04bc76fe70f11686e0264ef429
Micke Berg <micke.berg.dev@gmail.com>
2021-03-17 10:27:02 +0100

    Remove unused code

--- src/store.js
+++ src/store.js
@@ -1,24 +1,3 @@
-// import { createStore , applyMiddleware, compose ,combineReducers } from 'redux';
-// import thunk from 'redux-thunk';
-// import { productReducer } from "./reducers/productReducers";
-// import { cartReducer } from "./reducers/cartReducers";
-// import { orderReducer } from "./reducers/orderReducers";
-
-// const initialState = {};
-// const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-// const store = createStore(
-//   combineReducers({
-//     products: productReducer,
-//     cart: cartReducer,
-//     order: orderReducer,
-//     }),
-//     initialState,
-//     composeEnhancer(applyMiddleware(thunk))
-// );
-
-// export default store;
-
 import { createStore, applyMiddleware, compose, combineReducers } from "redux";
 import thunk from "redux-thunk";
 import { productsReducer } from "./reducers/productReducers";
