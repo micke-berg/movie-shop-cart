6ef48cdba89bf9e5f31dd27813df5145a2690493
Micke Berg <micke.berg.dev@gmail.com>
2021-03-17 20:51:26 +0100

    New genreAction

---
+++ src/actions/genreActions.js
@@ -0,0 +1,32 @@
+import { FETCH_GENRES, FILTER_PRODUCTS_BY_GENRE } from '../types';
+import axios from "axios";
+
+export const fetchGenresAction = () => async (dispatch) => {
+  await axios.get("/genre")
+  .then((res) => {
+      console.log('movieGenres', res.data.data)
+    dispatch({ 
+      type: FETCH_GENRES, 
+      payload: res.data.data || []
+    });
+  }, (error) => {
+    console.log(error);
+  })
+};
+
+// TODO get this to work
+export const filterProductsByGenreAction = (products, genre) => (dispatch) => {
+  console.log('filteredProducts:', products);
+  console.log('filteredProductsGenre:', genre);
+
+  dispatch({
+    type: FILTER_PRODUCTS_BY_GENRE,
+    payload: {
+      genre: genre,
+      items: 
+        genre === ""
+          ? products
+          : products.filter((x) => x.genres.indexOf(genre) >= 0),
+    },
+  });
+};
